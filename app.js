4bfeecbfb7f27f184dae801195340d4e89b9d417
Austin <joha0033@gmail.com>
2018-04-09 14:06:12 -0600

    added headers and body-parser

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const cors = require('cors')
-
+const bodyParser = require('body-parser')
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json())
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.use('/data', require('./routes/data'))
 
