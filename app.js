932d290a976c5e4409a3a3c9b6d8232ce363d44e
Austin <joha0033@gmail.com>
2018-04-09 14:35:03 -0600

    cors still an issue

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
 const app = express()
 
-app.use(cors())
-app.use(bodyParser.json())
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('dev'));
+
+
+// app.all('/*', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+// });
+
 
 app.use('/data', require('./routes/data'))
+
 
 const port = process.env.PORT || 5000;
 
